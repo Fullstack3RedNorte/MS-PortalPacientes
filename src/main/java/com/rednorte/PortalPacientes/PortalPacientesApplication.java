49570b76cd62f561a792b40c3181b9a21d41b1ea
Franco Reyes <fr.reyesh@duocuc.cl>
2026-05-05 16:14:25 -0400

    first commit

---
+++ src/main/java/com/rednorte/PortalPacientes/PortalPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.rednorte.PortalPacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalPacientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rednorte/PortalPacientes/PortalPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rednorte.PortalPacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
